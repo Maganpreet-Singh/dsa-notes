5bb4fb7564765a68f0fcd95f3f291ae16238428e
MAGANPREET SINGH <as1783268@gmail.com>
2026-06-27 11:54:40 +0530

    Add files via upload

---
+++ Intoduction To Hashing/Store Frequency in Dictionary.py
@@ -0,0 +1,22 @@
+# Manual Iteration Approach
+nums = [5,6,7,7,1,9,111,1,1,5,1,1]
+freq = {}
+for i in range(0,len(nums)):
+    if nums[i] in freq:
+        freq[nums[i]] += 1
+    else:
+        freq[nums[i]] = 1
+print(freq)
+
+# Time Complexity = O(N)
+# Space Complexity = O(N)
+
+# Using .get() Method
+hash_map = {}
+n = len(nums)
+for i in range(0,n):
+    hash_map[nums[i]] = hash_map.get(nums[i], 0) + 1
+print(hash_map)
+
+# Time Complexity = O(N)
+# Space Complexity = O(N)
